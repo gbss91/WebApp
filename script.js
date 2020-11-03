5d0e8292b937d5a7357abe7c731b8953338722c5
gbss91 <gbss91@gmail.com>
2020-11-03 00:34:44 +0000

    Responsive navbar and JS file

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+//Adds responsive class to css to show responsive menu
+function navButton() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
